a4d2a809688523222cf653842442abe2612dbaef
Andrew Maher <amaher423@gmail.com>
2023-04-19 16:27:28 -0500

    Added admin features, models for posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Andrew',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 4, 2023'
-    },
-    {
-        'author': 'James',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 5, 2023'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
